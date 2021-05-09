41771e5910d630ed9e200feeb1ddd55e5747163d
PikaChuBolt <kingtaym@gmail.com>
2021-05-09 13:56:52 -0400

    Update evac.py

--- evac.py
+++ evac.py
@@ -30,12 +30,12 @@ row = cur.fetchone()
 while row is not None:
     i = i + 1
     parid = str(row[0])
-    sql2 = "select p.parid::integer, p.geom, ST_Distance(p.geom, (select p2.geom from volusia.parcel p2 where p2.parid=" + parid + "))/5280  from volusia.parcel p where p.luc='2000' order by p.geom <-> (select p2.geom from volusia.parcel p2 where p2.parid=" + parid + ") limit 1;"
+    sql2 = "select p.parid::integer, p.geom, ST_Distance(p.geom, (select p2.geom from volusia.parcel p2 where p2.parid=" + parid + "))/5280 from volusia.parcel p where p.luc='0000' or p.luc='0100' or p.luc='0200' or p.luc='0400' or p.luc='0800' order by p.geom <-> (select p2.geom from volusia.parcel p2 where p2.parid=" + parid + ") limit 1;"
     cur2.execute(sql2)
     row2 = cur2.fetchone()
     parid2 = str(row2[0])
     distance = row2[2]
-    sql3 = "update volusia.parcel p1 set nearest_evac_zone = " + str(distance) + " where p1.parid=" + parid + ";"
+    sql3 = "update volusia.parcel p1 set nearest_evac_route = " + str(distance) + " where p1.parid=" + parid + ";"
     cur3.execute(sql3)
     if i%100 == 0:
         print(i)
